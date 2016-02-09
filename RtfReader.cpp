990861f2c5484330137a0a00499d82e69a7ab7b7
Xijian Yan <yanxijian@163.com>
2016-02-09 17:00:03 +0800

    Update RtfReader.cpp
    Udp

--- RtfReader.cpp
+++ RtfReader.cpp
@@ -11,7 +11,7 @@ bool RtfReader::RtfStream2TextString(string &outstring, istream &instream)
 		// "{\r" at the beginning, the RTF file is considered
 		char head[3] = { 0 };
 		instream.read(head, sizeof(head));
-		if (head[0] != '{' || head[1] != '\\' && head[2] != 'r')
+		if (head[0] != '{' || head[1] != '\\' || head[2] != 'r')
 		{
 			return false;
 		}
